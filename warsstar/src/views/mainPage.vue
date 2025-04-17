6d3bdb2b76b9ac15408da3c404886c50acff7975
Isadorq <iscapaticee@gmail.com>
2025-04-16 22:13:43 -0300

    .

--- warsstar/src/views/mainPage.vue
+++ warsstar/src/views/mainPage.vue
@@ -1,34 +1,65 @@
 <template>
-    <div class="navBar">
-      <p class="title">teste</p>
+  <div class="navBar">
+    <div class="logo">
+      <img :src="require('@/assets/bb8.png')" alt="Logo BB-8" />
     </div>
-  </template>
+  </div>
+
+  <div class="main">
+    <div class="containerMain">
+    <p class="test">Justin Bieber</p>
+  </div>
+  </div>
   
-  <script>
-  export default {
-    name: 'MainPage',
-  }
-  </script>
-  
-  <style lang="scss">
-  @import '@/assets/global.scss';
+</template>
 
-  body {
-    background-color: #0B0C10;
-  }
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
 
-  .navBar{
-    background-color: #3AA8F7;
-    height: 100px;
-    widht: auto;
-    disply: flex;
-    justify-items: center;
-    border: 3B00020;
-  }
+<style lang="scss">
+@import '@/assets/global.scss';
 
-  .title {
-    display: flex;
-    color: #F5F5F5;
-  }
+body {
+  background-color: #F8F8F8;
+  margin: 0;
+  padding: 0;
+}
 
-  </style>
+.navBar {
+  background-color: #F26522;
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo img {
+  width: 70px;
+  height: auto;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  padding: 100px;
+}
+
+.containerMain{
+  background-color: #B0B0B0;
+  display: flex;
+  justify-content: center;
+  height: 200px;
+  width: 100px;
+  border: 2px solid #585858;
+  border-radius: 5px;
+}
+
+.test {
+  color: white;
+}
+
+</style>
